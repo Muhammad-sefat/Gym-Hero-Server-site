545c40660fa29537f7f53c4fa0e42c7b8f1df3ec
Muhammad Sefat <muhammadsefat55@gmail.com>
2024-06-02 22:32:52 +0600

    Get Single data from trainers section

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 require("dotenv").config();
 const port = process.env.PORT || 5000;
@@ -44,6 +44,14 @@ async function run() {
     // get all trainers
     app.get("/trainer", async (req, res) => {
       const result = await trainersCollection.find().toArray();
+      res.send(result);
+    });
+
+    // Get single data
+    app.get("/trainers/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await trainersCollection.findOne(query);
       res.send(result);
     });
     // Send a ping to confirm a successful connection
