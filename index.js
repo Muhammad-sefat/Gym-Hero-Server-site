580665e5f61175a958c7a07953c7620d3a22213a
Muhammad Sefat <muhammadsefat55@gmail.com>
2025-02-12 09:10:07 +0600

    Final commit

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
     const reviewsCollection = client.db("GymHero").collection("review");
     const trainersCollection = client.db("GymHero").collection("trainer");
     const newsLettersCollection = client.db("GymHero").collection("newsLetter");
